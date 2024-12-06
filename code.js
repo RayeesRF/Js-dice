fd7f36547c5a76d8e363cbeac6fd55b6c0d2bcfe
Its_Me_RayeesRF <fathimarayeesa276@gmail.com>
2024-12-06 21:08:07 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,20 @@
+function roll() {
+  let dice = document.getElementById("dice");
+  let random = Math.floor(Math.random() * 6) + 1;
+  let audio = document.getElementById("diceAudio");
+  audio.play();
+  dice.classList.add("spin");
+  setTimeout(function () {
+    dice.classList.remove("spin");
+    dice.innerText = random;
+  }, 1000);
+}
+
+function createBox() {
+  let boxContainer = document.getElementById("boxContainer");
+  let div = document.createElement("div");
+
+  boxContainer.appendChild(div);
+
+  div.classList.add("box");
+}
